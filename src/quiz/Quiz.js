d1a61c7e756ff4e69e953320258f019def4d6ad3
Lucas Melo <luucasmell@gmail.com>
2019-04-23 13:48:02 -0300

    Create logic at buttons and install react bootstrap to use tooltips

--- src/quiz/Quiz.js
+++ src/quiz/Quiz.js
@@ -1,11 +1,40 @@
 import React,  { Component } from 'react';
 import logoMarvel from '../assets/images/marvelLogo.jpg';
 import thorImage from '../assets/images/thorImage.png';
+import Overlay from 'react-bootstrap/Overlay'
 
 import './Quiz.css';
 
 
 class Quiz extends Component {
+  constructor(...args){
+    super(...args)
+
+    this.attachRef = target => this.setState({ target });
+    this.state={
+      contador: 0,
+      show: false
+    }
+  }
+
+  toIncreaseContador =() =>{
+    this.setState({
+      contador: this.state.contador +1,
+    });
+  }
+
+
+  toDecrementContador = () =>{
+    this.setState({
+      contador: this.state.contador -1,
+    })
+  }
+
+  toClearContador = () => {
+    this.setState({
+      contador: 0,
+    })
+  }
 
   render() {
     return (
@@ -14,7 +43,7 @@ class Quiz extends Component {
             <img className="logoQuiz" src={logoMarvel} alt="Logo quiz Marvel"/>
             <h1>Quiz</h1>
             <p>Sua pontuação é:</p>
-            <p>0</p>
+            <p>{this.state.contador}</p>
 
             {/* ÁREA DE GERAÇÃO DAS IMAGENS DO QUIZ */}
             <section>
@@ -22,21 +51,21 @@ class Quiz extends Component {
             </section>
 
             {/* INPUT DE INSERÇÃO DA RESPOSTA */}
-            <form>
+            <section>
               <div>
                 <input type="text" placeholder="Digite sua resposta"/>
               </div>
 
               {/* BOTOES DE RESPOSTA AJUDA E DESISTENCIA */}
               <div>
-                <input id="answerButton" type="submit" value="Responder" />
+                <button id="answerButton" onClick={this.toIncreaseContador}>Responder</button>
               </div>
               
               <div id="helpButtons">
-                <input type="submit" value="Mostrar resposta" />
-                <input type="submit" value="Dica" />
+                <input type="submit" value="Mostrar resposta" onClick={this.toClearContador}/>
+                <input type="submit" value="Dica" onClick={this.toDecrementContador}/>
               </div>
-            </form>
+            </section>
           </header>
       </div>
     )
